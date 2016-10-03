6b628e018b5bac56ce499433d024ffd19bb01c8f
gonzardain <zardainmuzik@gmail.com>
2016-10-03 16:05:53 -0500

    modificado:    clients/urls.py 	modificado:    clients/urls.pyc 	modificado:    clients/views.py 	modificado:    clients/views.pyc 	modificado:    html/base.html 	modificado:    html/clients/index.html 	nuevo archivo: html/clients/new_form.html 	modificado:    settings/settings.pyc

--- clients/views.py
+++ clients/views.py
@@ -3,4 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 
 def list_clients(request):
+	flag = False
+	title = "Clientes" 
 	return render(request, 'clients/index.html', locals())
+
+
+def new_clients(request):
+	title = "Agregar Nuevo Cliente" 
+	return render(request, 'clients/new_form.html', locals())
